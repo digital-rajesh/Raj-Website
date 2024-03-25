c6a5566af05bb743a57dc6331bf822d7b9f293b3
digital-rajesh <digitalrajesh24@gmail.com>
2024-03-25 08:34:00 +0545

    code fixes

--- raj.js
+++ raj.js
@@ -1,8 +1,7 @@
-var typed = new typed(".text", {
-    Strings: ["Web-Developer", "Graphic Designer","Digital Marketer"],
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Graphic Designer", "Digital Marketer"],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1000,                                                                               
-    
-    looped: true
+    backDelay: 1000,
+    loop: true
 });
