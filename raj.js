604119110d1982300e5bff7b0c9ae3d132568508
digital-rajesh <digitalrajesh24@gmail.com>
2024-03-25 08:28:51 +0545

    WEbsite Push

---
+++ raj.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text", {
+    Strings: ["Web-Developer", "Graphic Designer","Digital Marketer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,                                                                               
+    
+    looped: true
+});
---
+++ raj.js.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: ["Frontend Developer", "Youtuber", "Web Developer"],                                                               +["Frontend Developer","Graphics Designer","Digital Marketer"],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    looped : true
+});
